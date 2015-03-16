d6d6e8ab11f09cb509da6fcb0334abdff532e9ac
SATYAM GUPTA <s52gupta@uwaterloo.ca>
2015-03-16 02:57:35 -0400

    initial commit to gather valence scores on the review data. simple stat on percentage of words found in the valence dataset also available

---
+++ src/valence_data.py
@@ -0,0 +1,33 @@
+_DEBUG = 0
+
+class TextReader:
+	def __init__(self, file_name):
+		self._file_name = file_name
+
+	def open(self):
+		self._file_object = open(self._file_name, 'r')
+
+	def close(self):
+		self._file_object.close()
+
+	def read(self):
+		for line in self._file_object:
+			yield line
+
+class ValenceData:
+	def __init__(self, file_name):
+		self._tr = TextReader(file_name)
+		self._tr.open()
+		self._data_map = {}
+
+	def process_data(self):
+		for line in self._tr.read():
+			[word, valence_score] = line.split('\t')
+			self._data_map[word] = valence_score
+
+if _DEBUG:
+	valence_data = ValenceData('./data/valence.txt')
+	valence_data.process_data()
+
+	for k, v in valence_data._data_map.items():
+		print k, v
