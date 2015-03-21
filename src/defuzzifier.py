453a71ce80deae6d153906748448535972940cd6
Eddie Kibicho <ekibicho@uwaterloo.ca>
2015-03-21 15:31:17 -0400

    Put Defuzzifier into a class

--- src/defuzzifier.py
+++ src/defuzzifier.py
@@ -1,7 +1,21 @@
-def defuzzifier(f, start, end, step_size=1e-3):
-    wieght_sum = 0
-    total_sum =0
-    for x in xrange(start, end, step_size):
-        total_sum += f(x) * x
-        wieght_sum += f(x)
-    return total_sum/wieght_sum
+class Defuzzifier:
+
+    def __init__(self):
+        pass
+    def xfrange(self, start, stop, step):
+        while start < stop:
+            yield start
+            start += step
+    def defuzzify(self, f, start, end, step_size=1e-3):
+        weight_sum = 0
+        total_sum =0
+        for x in self.xfrange(start, end, step_size):
+            total_sum += f(x) * x
+            weight_sum += f(x)
+        if weight_sum > 0 :
+            return total_sum/weight_sum
+        else :
+            return 0
+
+    def __call__(self,*args,**kwargs):
+         return self.defuzzify(*args,**kwargs)
